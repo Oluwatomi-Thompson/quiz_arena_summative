204e4f779f75396a0d8e2d8b9ccc9318c8611b2a
lilianeuwase2 <l.uwase@alustudent.com>
2025-07-17 16:00:51 +0200

    user details file

--- user.py
+++ user.py
@@ -8,7 +8,20 @@ def create_user():
         if username == "":
             print("the name can not be empty. Please try again!")
             continue
-            
 
+        #check if username is already taken
+        cursor = conn.execute("SELECT * FROM users WHERE username = ?", (username,))
+
+        if cursor.fetchone() is not None:
+            print(f"Sorry, {username} is taken. choose another name")
+            else 
+            #save to the database
+            conn.execute("INSERT INTO users (username) values (?)", (username,))
+            conn.commit()
+            print(f"account created! Welcome {username}")
+            break
+
+    conn.close()
+    
 
         

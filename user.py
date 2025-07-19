6a8f296e8ed9de8e86dd38322efc160be586b1d2
lilianeuwase2 <l.uwase@alustudent.com>
2025-07-19 22:57:40 +0200

    new changes

--- user.py
+++ user.py
@@ -1,27 +1,10 @@
-def create_user():
-    print("Welcome to QuizArena")
+from splite3 import error 
+from utils import validate_username 
 
+def create_user(conn):
+    cursor = conn.cursor()
     while True:
-        username = input("enter your name for the game: ").strip()
-
-        #check if empty
-        if username == "":
-            print("the name can not be empty. Please try again!")
+        username = input("Enter your nickname for the game:").strip()
+        if not validate_username(username):
             continue
-
-        #check if username is already taken
-        cursor = conn.execute("SELECT * FROM users WHERE username = ?", (username,))
-
-        if cursor.fetchone() is not None:
-            print(f"Sorry, {username} is taken. choose another name")
-            else 
-            #save to the database
-            conn.execute("INSERT INTO users (username) values (?)", (username,))
-            conn.commit()
-            print(f"account created! Welcome {username}")
-            break
-
-    conn.close()
-    
-
         
